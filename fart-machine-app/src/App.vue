32abd2933d57165c575887279a10e9da58e2cb92
zarahzachz <sarah.sachs@gmail.com>
2018-11-06 07:57:55 -0500

    Set up fart machine.

---
+++ fart-machine-app/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <button v-on:click="play()">Pull my finger</button>
+    <audio src="./assets/long_fart.mp3"></audio>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  methods: {
+    play: function () {
+      const audio = document.body.querySelector('audio');
+      audio.play();
+    },
+  },
+};
+</script>
+
+<style>
+</style>
